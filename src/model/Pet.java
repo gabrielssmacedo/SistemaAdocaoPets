d3c730fc570527d43533f4ddb656136f9e7df74b
Gabriel Macedo <gabrielmacedo317@gmail.com>
2025-03-14 18:04:11 -0300

    feat: adiciona metodo cadastrar em Pet

--- src/model/Pet.java
+++ src/model/Pet.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 import model.enums.SexoPet;
 import model.enums.TipoPet;
 
@@ -11,6 +16,7 @@ public class Pet {
 	private Double idade;
 	private Double peso;
 	private String raca;
+	private LocalDateTime dataHora;
 	
 	public Pet(String nome) {
 		if(nome == null) System.out.println(); //lançar exceção
@@ -84,9 +90,45 @@ public class Pet {
 		this.raca = raca;
 	}
 	
-	public boolean cadastrar() {
-		///
-		///
+	public boolean cadastrar(String path) {
+		dataHora = LocalDateTime.now();
+		StringBuilder nomeArquivo = new StringBuilder();
+		
+		String[] nomes = this.nome.split(" ");
+		StringBuilder nomeMaiusculo = new StringBuilder();
+		for(String n : nomes) {
+			nomeMaiusculo.append(n.toUpperCase());
+		}
+		
+		nomeArquivo.append(String.valueOf(dataHora.getYear()));
+		nomeArquivo.append(String.valueOf(dataHora.getMonthValue()));
+		nomeArquivo.append(String.valueOf(dataHora.getDayOfMonth()));
+		nomeArquivo.append("T");
+		nomeArquivo.append(String.valueOf(dataHora.getHour()));
+		nomeArquivo.append(String.valueOf(dataHora.getMinute()));
+		nomeArquivo.append("-");
+		nomeArquivo.append(nomeMaiusculo.toString());
+		nomeArquivo.append(".txt");
+		
+		path = path + "\\" + nomeArquivo.toString();
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			bw.write(String.format("1 - %s\n", nome));
+			bw.write(String.format("2 - %s\n", tipo));
+			bw.write(String.format("3 - %s\n", sexo));
+			bw.write(String.format("4 - %s\n", 
+					  endereco.getRua() + ", "
+					+ String.valueOf(endereco.getNumero()) + ", "
+					+ endereco.getCidade()));
+			bw.write(String.format("5 - %.1f anos\n", idade));
+			bw.write(String.format("6 - %.1fKg\n", peso));
+			bw.write(String.format("7 - %s\n", raca));
+			return true;
+		}
+		catch(IOException e) {
+			System.out.println("Erro na geração do cadastro.");
+		}
+	
 		return false;
 	}
 	
